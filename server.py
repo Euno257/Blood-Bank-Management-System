6094ee85fb5a9232a1443127eb23f1bd85e2e38f
Euno257 <harshghlt25@gmail.com>
2019-10-22 00:19:09 +0530

    Added register.html(#04)

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, request, url_for, session, logging
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +11,20 @@ def index():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1,max=25)])
+    email = StringField('Email',[validators.Length(min=10,max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm',message='Password do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method  == 'POST' and form.validate():
+        return render_template('register.html')
+
+    return render_template('register.html',form = form)
