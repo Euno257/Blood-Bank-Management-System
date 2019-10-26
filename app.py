91efdff801b24218bf71dd6b00cacbd0adfe4a9d
Euno257 <harshghlt25@gmail.com>
2019-10-26 14:20:26 +0530

    login not working(#09)

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ def contact():
     return render_template('contact.html')
 
 class RegisterForm(Form):
-    name = StringField('Name', [validators.Length(min=1,max=25)])
-    email = StringField('Email',[validators.Length(min=10,max=50)])
+    name = StringField('Name', [validators.DataRequired(),validators.Length(min=1,max=25)])
+    email = StringField('Email',[validators.DataRequired(),validators.Length(min=10,max=50)])
     password = PasswordField('Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm',message='Password do not match')
@@ -51,9 +51,47 @@ def register():
         flashing_message = "Success! You can log in with Employee ID " + str(e_id)
         flash( flashing_message,"success")
 
-        return redirect(url_for('register'))
+        return redirect(url_for('login'))
 
     return render_template('register.html',form = form)
+
+#login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        e_id = request.form["e_id"]
+        password_candidate = request.form["password"]
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM RECEPTION WHERE E_ID = %s", [e_id])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['PASSWORD']
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['e_id'] = e_id
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('index'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            # Close connection
+            cur.close()
+        else:
+            error = 'Employee ID not found'
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
